cc9f7c5d57df27e52b71b7c06dd770cf141dc773
ask43 <askar_ra@mail.ru>
2021-11-01 15:38:57 +0600

    v2

--- routes/todos.js
+++ routes/todos.js
@@ -28,8 +28,7 @@ router.post('/create', async (req, res) => {
 
 router.post('/complete', async (req, res) => {
   const todo = await Todo.findById(req.body.id)
-  // console.log(req.body.id)
-  todo.completed = true
+  todo.completed = !!req.body.completed
   await todo.save()
   res.redirect('/')
 })
